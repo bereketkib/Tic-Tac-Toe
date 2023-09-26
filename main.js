3f92775bb815c85cce96bacab7d96d1cd62e2349
Bereket Kibreab <bekibreab@gmail.com>
2023-09-27 01:58:45 +0400

    created the 3 modules and functions

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-console.log("hi");
+const Gameboard = (() => {
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class = "square" id="square-${index}">${square}</div>`;
+        });
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click", Game.handleClick);
+        });
+    }
+
+    return {
+        render,
+    }
+
+})();
+
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+
+const Game = (() => {
+    let players =[];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+    }
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        console.log(index);
+    }
+
+    return {
+        start,
+        handleClick,
+    }
+
+})();
+
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    Game.start();
+});
